b6aa5183bd6741ccca6cb87b1de9d47010b25ed5
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-29 00:30:41 +0900

    Initil commit

---
+++ QiitaViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QiitaViewer
+//
+//  Created by Rudolph Miller on 2016/02/29.
+//  Copyright © 2016年 rudolph-miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
