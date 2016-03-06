0aac1ad667f71596fb082e7063ef3beb466a8797
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-03-06 21:23:35 +0900

    Use PagingViewController

--- QiitaViewer/PagingViewController.swift
+++ QiitaViewer/PagingViewController.swift
@@ -7,7 +7,25 @@
 //
 
 import UIKit
+import PagingMenuController
 
 class PagingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        let viewControllers = [0, 1, 2, 3, 4, 5, 6].map { (index) -> UIViewController in
+            let viewController = UIViewController()
+            viewController.title = "Tab: \(index)"
+            return(viewController)
+        }
+        let options = PagingMenuOptions()
+        let pagingMenuController = PagingMenuController(viewControllers: viewControllers, options: options)
+        pagingMenuController.view.frame.origin.y += 60
+        // options.menuDisplayMode = .Infinite(withMode: MenuItemWithMode)
+
+
+        self.addChildViewController(pagingMenuController)
+        self.view.addSubview(pagingMenuController.view)
+        pagingMenuController.didMoveToParentViewController(self)
+    }
 }
