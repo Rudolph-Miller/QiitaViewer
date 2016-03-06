8e881294f78d35ec344e65d8ee11198494e5c657
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-03-06 20:52:11 +0900

    Add new ViewController

---
+++ QiitaViewer/PagingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PagingViewController.swift
+//  QiitaViewer
+//
+//  Created by Rudolph Miller on 2016/03/06.
+//  Copyright © 2016年 rudolph-miller. All rights reserved.
+//
+
+import UIKit
+
+class PagingViewController: UIViewController {
+
+}
